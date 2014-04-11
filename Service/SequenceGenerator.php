2c93248b2a7eaee32724bb694c8229cba41ebecc
Carlos Mendoza <inhack20@tecnocreaciones.com>
2014-04-10 20:48:31 -0430

    Optimizando generador de secuencia de dolibar

--- Service/SequenceGenerator.php
+++ Service/SequenceGenerator.php
@@ -18,23 +18,24 @@ namespace Tecnocreaciones\Bundle\ToolsBundle\Service;
  */
 class SequenceGenerator implements \Symfony\Component\DependencyInjection\ContainerAwareInterface
 {
+    const MODE_NEXT = 0;
+    const MODE_LAST = 1;
+    
     private $container;
     
-    function generateNext() {
-        $doctrine = $this->getDoctrine();
-        $qb = $this->createQueryBuilder('q');
-        $qb->from('Tecnocreaciones\Vzla\EntityBundle\Entity\Country', 'q');
-        $table = 'tabla';
-        $field = 'description';
+    function generateLast(\Doctrine\ORM\QueryBuilder $qb,$field) {
         
-        $mode = 'next';
-        $cat = 'CATEGORIA';
-        $zone = 'ZONA';
-        $mask = 'SIEMPRE-{dd}-{mm}-{yyyy}-{yy}-{000}';
-        // Clean parameters
-        $date = time(); // We use local year and month of PHP server to search numbers
+    }
+    
+    function generate(\Doctrine\ORM\QueryBuilder $qb, $mask,$field,$mode = self::MODE_NEXT,$parameters = array()) 
+    {
+        $aliases = $qb->getRootAliases();
+        $alias = $aliases[0];
+        $field = $alias.'.'.$field;
+        if($mode === null){
+            $mode = self::MODE_NEXT;
+        }
             
-        // but we should use local year and month of user
         // Extract value for mask counter, mask raz and mask offset
         if (!preg_match('/\{(0+)([@\+][0-9]+)?([@\+][0-9]+)?\}/i', $mask, $reg))
             return 'ErrorBadMask';
@@ -43,7 +44,8 @@ class SequenceGenerator implements \Symfony\Component\DependencyInjection\Contai
         $maskraz = -1;
         $maskoffset = 0;
         if (strlen($maskcounter) < 2)
-            return 'CounterMustHaveMoreThan2Digits';
+            return 'CounterMustHaveMoreThan3Digits';
+        
         $maskrefclient_maskcounter = '';
         $maskrefclient = '';
 
@@ -56,177 +58,71 @@ class SequenceGenerator implements \Symfony\Component\DependencyInjection\Contai
         $maskwithonlyymcode = preg_replace('/\{(c+)(0*)\}/i', $maskrefclient, $maskwithonlyymcode);
         $maskwithonlyymcode = preg_replace('/\{(t+)\}/i', $masktype_value, $maskwithonlyymcode);
         $maskwithnocode = $maskwithonlyymcode;
+        $defaultMask = new \Doctrine\Common\Collections\ArrayCollection(array(
+            'yyyy','yy','mm'
+        ));
         $maskwithnocode = preg_replace('/\{yyyy\}/i', 'yyyy', $maskwithnocode);
-        $maskwithnocode = preg_replace('/\{CAT\}/i', 'yyyy', $maskwithnocode);
-        $maskwithnocode = preg_replace('/\{ZONE\}/i', 'yyyy', $maskwithnocode);
         $maskwithnocode = preg_replace('/\{yy\}/i', 'yy', $maskwithnocode);
-        $maskwithnocode = preg_replace('/\{y\}/i', 'y', $maskwithnocode);
         $maskwithnocode = preg_replace('/\{mm\}/i', 'mm', $maskwithnocode);
-        // Now maskwithnocode = 0000ddmmyyyyccc for example
-        // and maskcounter    = 0000 for example
-        //print "maskwithonlyymcode=".$maskwithonlyymcode." maskwithnocode=".$maskwithnocode."\n<br>";
         // If an offset is asked
         if (!empty($reg[2]) && preg_match('/^\+/', $reg[2]))
             $maskoffset = preg_replace('/^\+/', '', $reg[2]);
         if (!empty($reg[3]) && preg_match('/^\+/', $reg[3]))
             $maskoffset = preg_replace('/^\+/', '', $reg[3]);
-
-        // Define $sqlwhere
-        // If a restore to zero after a month is asked we check if there is already a value for this year.
-        if (!empty($reg[2]) && preg_match('/^@/', $reg[2]))
-            $maskraz = preg_replace('/^@/', '', $reg[2]);
-        if (!empty($reg[3]) && preg_match('/^@/', $reg[3]))
-            $maskraz = preg_replace('/^@/', '', $reg[3]);
-        if ($maskraz >= 0) {
-            if ($maskraz > 12)
-                return 'ErrorBadMaskBadRazMonth';
-
-            // Define posy, posm and reg
-            if ($maskraz > 1) {
-                if (!preg_match('/^(.*)\{(y+)\}\{(m+)\}/i', $maskwithonlyymcode) && !preg_match('/^(.*)\{(m+)\}\{(y+)\}/i', $maskwithonlyymcode))
-                    return 'ErrorCantUseRazInStartedYearIfNoYearMonthInMask';
-                if (preg_match('/^(.*)\{(y+)\}\{(m+)\}/i', $maskwithonlyymcode, $reg)) {
-                    $posy = 2;
-                    $posm = 3;
-                } elseif (preg_match('/^(.*)\{(m+)\}\{(y+)\}/i', $maskwithonlyymcode, $reg)) {
-                    $posy = 3;
-                    $posm = 2;
-                }
-                if (strlen($reg[$posy]) < 2)
-                    return 'ErrorCantUseRazWithYearOnOneDigit';
-            }
-            else {
-                if (!preg_match('/^(.*)\{(y+)\}/i', $maskwithonlyymcode))
-                    return 'ErrorCantUseRazIfNoYearInMask';
-                if (preg_match('/^(.*)\{(y+)\}/i', $maskwithonlyymcode, $reg)) {
-                    $posy = 2;
-                    $posm = 0;
-                }
-            }
-            //print "x".$maskwithonlyymcode." ".$maskraz." ".$posy." ".$posm;
-            // Define $yearcomp and $monthcomp (that will be use in the select where to search max number)
-            $sqlwhere = '';
-            $monthcomp = $maskraz;
-            $yearoffset = 0;
-            $yearcomp = 0;
-            if (date("m", $date) < $maskraz) {
-                $yearoffset = -1;
-            } // If current month lower that month of return to zero, year is previous year
-            if (strlen($reg[$posy]) == 4)
-                $yearcomp = sprintf("%04d", date("Y", $date) + $yearoffset);
-            if (strlen($reg[$posy]) == 2)
-                $yearcomp = sprintf("%02d", date("y", $date) + $yearoffset);
-            if (strlen($reg[$posy]) == 1)
-                $yearcomp = substr(date("y", $date), 2, 1) + $yearoffset;
-            $sqlwhere = '';
-            $sqlwhere.='( (SUBSTRING(' . $field . ', ' . (strlen($reg[1]) + 1) . ', ' . strlen($reg[2]) . ') >= ' . $yearcomp;
-            if ($monthcomp > 1) { // Test useless if monthcomp = 1 (or 0 is same as 1)
-                if (strlen($reg[$posy]) == 4)
-                    $yearcomp1 = sprintf("%04d", date("Y", $date) + $yearoffset + 1);
-                if (strlen($reg[$posy]) == 2)
-                    $yearcomp1 = sprintf("%02d", date("y", $date) + $yearoffset + 1);
-                // FIXME If mask is {mm}{yy}, sqlwhere is wrong here
-                $sqlwhere.=' AND SUBSTRING(' . $field . ', ' . (strlen($reg[1]) + strlen($reg[2]) + 1) . ', ' . strlen($reg[3]) . ') >= ' . $monthcomp . ')';
-                $sqlwhere.=' OR SUBSTRING(' . $field . ', ' . (strlen($reg[1]) + 1) . ', ' . strlen($reg[2]) . ') >= ' . $yearcomp1 . ' )';
-            }
-            else {
-                $sqlwhere.=') )';
-            }
-        }
-        //print $sqlwhere;
-        //print "masktri=".$masktri." maskcounter=".$maskcounter." maskraz=".$maskraz." maskoffset=".$maskoffset." yearcomp=".$yearcomp."<br>\n";
-        // Define $sqlstring
-        // 
-        //TODO en caso de productos no funciona bien (toma los ultimos 4 digitos)
+        
         $posnumstart = strpos($maskwithnocode, $maskcounter); // Pos of counter in final string (from 0 to ...)
-        if ($posnumstart < 0)
-            return 'ErrorBadMaskFailedToLocatePosOfSequence';
-        $sqlstring = 'SUBSTRING(' . 'q.'.$field . ', ' . ($posnumstart + 1) . ', ' . strlen($maskcounter) . ')';
-        //print "x".$sqlstring;
-        // Define $maskLike
+        $sqlstring = 'SUBSTRING(' .$field . ', ' . ($posnumstart + 1) . ', ' . strlen($maskcounter) . ')';
         $maskLike = trim($mask);
         $maskLike = str_replace("%", "_", $maskLike);
         // Replace protected special codes with matching number of _ as wild card caracter
         $maskLike = preg_replace('/\{yyyy\}/i', '____', $maskLike);
-        $maskLike = preg_replace('/\{CAT\}/i', $cat, $maskLike);
-        $maskLike = preg_replace('/\{ZONE\}/i', $zone, $maskLike);
         $maskLike = preg_replace('/\{yy\}/i', '__', $maskLike);
-        $maskLike = preg_replace('/\{y\}/i', '_', $maskLike);
         $maskLike = preg_replace('/\{mm\}/i', '__', $maskLike);
         $maskLike = preg_replace('/\{dd\}/i', '__', $maskLike);
         $maskLike = str_replace($this->dol_string_nospecial('{' . $masktri . '}'), str_pad("", strlen($maskcounter), "_"), $maskLike);
-        var_dump($maskLike);
-        if ($maskrefclient)
-            $maskLike = str_replace($this->dol_string_nospecial('{' . $maskrefclient . '}'), str_pad("", strlen($maskrefclient), "_"), $maskLike);
-        //if ($masktype) $maskLike = str_replace($this->dol_string_nospecial('{'.$masktype.'}'),str_pad("",strlen($masktype),"_"),$maskLike);
-        if ($masktype)
-            $maskLike = str_replace($this->dol_string_nospecial('{' . $masktype . '}'), $masktype_value, $maskLike);
-
+        
         // Get counter in database
         $counter = 0;
         $qb->select('MAX('.$sqlstring.') as v')
-                //->setParameter('1', $sqlstring)
                 ;
-        $qb->where($qb->expr()->like('q.'.$field, "'".$maskLike."'"));
-        $qb->andWhere($qb->expr()->notLike('q.'.$field, "'%PROV%'"));
-        $sql = "SELECT MAX(" . $sqlstring . ") as val";
-        $sql.= " FROM " . $table;
-        //		$sql.= " WHERE ".$field." not like '(%'";
-//        $sql.= " WHERE " . Container::getDB()->like($field, '' . $maskLike . '') . "";
-//        $sql.= " AND " . Container::getDB()->like($field . ' NOT ', '%PROV%') . "";
-//        if (isset($sqlwhere))
-//            $sql.=' AND ' . $sqlwhere;
-        var_dump($sqlstring);
-        var_dump($qb->getQuery()->getSQL());
+        $qb->where($qb->expr()->like($field, "'".$maskLike."'"));
+        $qb->andWhere($qb->expr()->notLike($field, "'%PROV%'"));
+        
         $result = $qb->getQuery()->getOneOrNullResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
-        echo "Result";
-        var_dump($result);
         if ($result) {
             $counter = $result['v'];
         }
         if (empty($counter) || preg_match('/[^0-9]/i', $counter))
             $counter = $maskoffset;
-
-        if ($mode == 'last') {
-            
-        }
-        else if ($mode == 'next') {
+        //$counter+=$maskoffset;
+        
+        if ($mode == self::MODE_NEXT) {
             $counter++;
         }
-            // Build numFinal
-            $numFinal = $mask;
-
-            // We replace special codes except refclient
-            $numFinal = preg_replace('/\{yyyy\}/i', date("Y", $date), $numFinal);
-            $numFinal = preg_replace('/\{CAT\}/i', $cat, $numFinal);
-
-            $numFinal = preg_replace('/\{ZONE\}/i', $zone, $numFinal);
-            $numFinal = preg_replace('/\{yy\}/i', date("y", $date), $numFinal);
-            $numFinal = preg_replace('/\{y\}/i', substr(date("y", $date), 2, 1), $numFinal);
-            $numFinal = preg_replace('/\{mm\}/i', date("m", $date), $numFinal);
-            $numFinal = preg_replace('/\{dd\}/i', date("d", $date), $numFinal);
-
-            // Now we replace the counter
-            $maskbefore = '{' . $masktri . '}';
-            $maskafter = str_pad($counter, strlen($maskcounter), "0", STR_PAD_LEFT);
-            //print 'x'.$maskbefore.'-'.$maskafter.'y';
-            $numFinal = str_replace($maskbefore, $maskafter, $numFinal);
-
-            // Now we replace the refclient
-            if ($maskrefclient) {
-                //print "maskrefclient=".$maskrefclient." maskwithonlyymcode=".$maskwithonlyymcode." maskwithnocode=".$maskwithnocode."\n<br>";
-                $maskrefclient_maskbefore = '{' . $maskrefclient . '}';
-                $maskrefclient_maskafter = $maskrefclient_clientcode . str_pad($maskrefclient_counter, strlen($maskrefclient_maskcounter), "0", STR_PAD_LEFT);
-                $numFinal = str_replace($maskrefclient_maskbefore, $maskrefclient_maskafter, $numFinal);
+        // Build numFinal
+        $numFinal = $mask;
+        $date = new \DateTime();
+        // We replace special codes except refclient
+        $numFinal = preg_replace('/\{yyyy\}/i',$date->format("Y"), $numFinal);
+        $numFinal = preg_replace('/\{yy\}/i', $date->format("y"), $numFinal);
+        $numFinal = preg_replace('/\{mm\}/i', $date->format("m"), $numFinal);
+        $numFinal = preg_replace('/\{dd\}/i', $date->format("d"), $numFinal);
+        
+        foreach ($this->getAdditionalMasks() as $additionalMask) {
+            if(isset($parameters[$additionalMask])){
+                $numFinal = preg_replace('/\{'.$additionalMask.'\}/i', $parameters[$additionalMask], $numFinal);
             }
-
-            // Now we replace the type
-            if ($masktype) {
-                $masktype_maskbefore = '{' . $masktype . '}';
-                $masktype_maskafter = $masktype_value;
-                $numFinal = str_replace($masktype_maskbefore, $masktype_maskafter, $numFinal);
-            }
+        }
+        // Now we replace the counter
+        $maskbefore = '{' . $masktri . '}';
+        $maskafter = str_pad($counter, strlen($maskcounter), "0", STR_PAD_LEFT);
+        $numFinal = str_replace($maskbefore, $maskafter, $numFinal);
+        
         return $numFinal;
+    }
+    
+    function generateNext(\Doctrine\ORM\QueryBuilder $qb,$mask,$field,$parameters = array()) {
+        return $this->generate($qb, $mask,$field,self::MODE_NEXT,$parameters);
     }
     
     /**
@@ -254,7 +150,7 @@ class SequenceGenerator implements \Symfony\Component\DependencyInjection\Contai
      * @param type $alias
      * @return \Doctrine\ORM\QueryBuilder
      */
-    private function createQueryBuilder($alias) {
+    public final function createQueryBuilder($alias = 'q') {
          return $this->getDoctrine()->getManager()->createQueryBuilder($alias);
     }
     
@@ -266,5 +162,9 @@ class SequenceGenerator implements \Symfony\Component\DependencyInjection\Contai
             //$forbidden_chars_to_remove=array("(",")");
 
             return str_replace($forbidden_chars_to_replace,$newstr,str_replace($forbidden_chars_to_remove,"",$str));
+    }
+    
+    function getAdditionalMasks() {
+        return array('CAT','ZONE');
     }
 }
