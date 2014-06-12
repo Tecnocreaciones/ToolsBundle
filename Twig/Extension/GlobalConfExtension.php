034639c964bee02627f7aa088ac55da47a615763
Carlos Mendoza <inhack20@tecnocreaciones.com>
2014-06-12 18:44:45 -0430

    Agregando funcion a twig

--- Twig/Extension/GlobalConfExtension.php
+++ Twig/Extension/GlobalConfExtension.php
@@ -16,21 +16,79 @@ namespace Tecnocreaciones\Bundle\ToolsBundle\Twig\Extension;
  *
  * @author Carlos Mendoza <inhack20@tecnocreaciones.com>
  */
-class GlobalConfExtension extends \Twig_Extension implements \Symfony\Component\DependencyInjection\ContainerAwareInterface
-{
+class GlobalConfExtension extends \Twig_Extension implements \Symfony\Component\DependencyInjection\ContainerAwareInterface {
+
     private $container;
-    
+
     public function getGlobals() {
         return array('appConfiguration' => $this->container->get('tecnocreaciones_tools.configuration_service'));
     }
-    
-    public function getName()
-    {
+
+    public function getName() {
         return 'tecnocreaciones_tools_global_conf_extension';
     }
 
-    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null)
+    public function getFilters()
     {
+        return array(
+            new \Twig_SimpleFilter('clearString', array($this, 'clearString')),
+        );
+    }
+
+    /**
+     * Reemplaza todos los acentos por sus equivalentes sin ellos
+     *
+     * @param $string
+     *  string la cadena a sanear
+     *
+     * @return $string
+     *  string saneada
+     */
+    function clearString($string)
+    {
+
+        $string = trim($string);
+
+        $string = str_replace(
+                array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'), array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'), $string
+        );
+
+        $string = str_replace(
+                array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'), array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'), $string
+        );
+
+        $string = str_replace(
+                array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'), array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'), $string
+        );
+
+        $string = str_replace(
+                array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'), array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'), $string
+        );
+
+        $string = str_replace(
+                array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'), array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'), $string
+        );
+
+        $string = str_replace(
+                array('ñ', 'Ñ', 'ç', 'Ç'), array('n', 'N', 'c', 'C',), $string
+        );
+
+        //Esta parte se encarga de eliminar cualquier caracter extraño
+        $string = str_replace(
+                array("\\", "¨", "º", "-", "~",
+            "#", "@", "|", "!", "\"",
+            "·", "$", "%", "&", "/",
+            "(", ")", "?", "'", "¡",
+            "¿", "[", "^", "`", "]",
+            "+", "}", "{", "¨", "´",
+            ">", "< ", ";", ",", ":",
+            ".", " "), '', $string
+        );
+
+        return $string;
+    }
+
+    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null) {
         $this->container = $container;
     }
 
