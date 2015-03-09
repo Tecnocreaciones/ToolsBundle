9f0cf9cf3c27295470e954b9d47535b12a18af29
Carlos Mendoza <inhack20@gmail.com>
2015-03-09 13:21:49 -0430

    Agregando unidad de bolivares

---
+++ Service/UnitConverter/Type/CurrencyUnitType.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnocreaciones\Bundle\ToolsBundle\Service\UnitConverter\Type;
+
+use Tecnocreaciones\Bundle\ToolsBundle\Service\UnitConverter\UnitType;
+
+/**
+ * Moneda
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class CurrencyUnitType extends UnitType
+{
+    const UNIT_BOLIVAR = 'bolivar';
+    const UNIT_DOLLAR = 'dollar';
+    
+    public function getDescription()
+    {
+        return 'Moneda';
+    }
+    
+    public function init() {
+        $this->insertUnit(self::UNIT_BOLIVAR, 'bs', 6.30);
+        $this->insertUnit(self::UNIT_DOLLAR, '$', 6.30);
+    }
+    
+    public static function getType() {
+        return 'currency';
+    }
+}
