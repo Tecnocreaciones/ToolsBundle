30d29489041f7395068af29d26e31bb1c275f452
Carlos Mendoza <inhack20@gmail.com>
2015-06-17 09:57:04 -0430

    Agregando paginador para datatables

---
+++ Model/Paginator/Paginator.php
@@ -0,0 +1,138 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com.ve
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnocreaciones\Bundle\ToolsBundle\Model\Paginator;
+
+use Pagerfanta\Pagerfanta as BasePagerfanta;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Router;
+
+/**
+ * Pagerfanta modificado para serializarlo
+ *
+ * @author Anais Ortega <adcom23@tecnocreaciones.com.ve>
+ */
+class Paginator extends BasePagerfanta implements ContainerAwareInterface
+{
+    protected $route = null;
+    protected $container;
+    protected $defaultFormat = self::FORMAT_ARRAY_DEFAULT;
+    protected $draw = 1;
+    /**
+     *
+     * @var \Symfony\Component\HttpFoundation\Request 
+     */
+    protected $request;
+
+    /**
+     * Devuelve un formato estandar de trabajo
+     */
+    const FORMAT_ARRAY_DEFAULT = 'default';
+    
+    /**
+     * Devuelve un formato para que pueda ser leido por el plugin DataTables de jQuery
+     */
+    const FORMAT_ARRAY_DATA_TABLES = 'dataTables';
+    
+    private $formatArray = array(
+        self::FORMAT_ARRAY_DEFAULT,self::FORMAT_ARRAY_DATA_TABLES
+    );
+            
+    function formatToArrayDefault($route = null,array $parameters = array()) {
+        $links = array(
+            'self'  => array('href' => ''),
+            'first' => array('href' => ''),
+            'last'  => array('href' => ''),
+            'next'  => array('href' => ''),
+            'previous'  => array('href' => ''),
+        );
+        $paginator = array(
+                        'getNbResults' => $this->getNbResults(),
+                        'getCurrentPage' => $this->getCurrentPage(),
+                        'getNbPages' => $this->getNbPages(),
+                        'getMaxPerPage' => $this->getMaxPerPage(),
+                    );
+        if($route != null){
+            $links['first']['href'] = $this->generateUrl($route, array_merge($parameters, array('page' => 1)));
+            $links['self']['href'] = $this->generateUrl($route, array_merge($parameters, array('page' => $this->getCurrentPage())));
+            $links['last']['href'] = $this->generateUrl($route, array_merge($parameters, array('page' => $this->getNbPages())));
+            if($this->hasPreviousPage()){
+                $links['previous']['href'] = $this->generateUrl($route, array_merge($parameters, array('page' => $this->getPreviousPage())));
+            }
+            if($this->hasNextPage()){
+                $links['next']['href'] = $this->generateUrl($route, array_merge($parameters, array('page' => $this->getNextPage())));
+            }
+        }
+        $pageResult = $this->getCurrentPageResults();
+        if(is_array($pageResult)){
+            $results = $pageResult;
+        }else{
+            $results = $this->getCurrentPageResults()->getArrayCopy();
+        }
+        return array(
+            '_links' => $links,
+            '_embedded' => array(
+                'results' => $results,
+                'paginator' => $paginator
+            ),
+        );
+    }
+    
+    function formatToArrayDataTables($route = null,array $parameters = array()) {
+        $results = $this->getCurrentPageResults()->getArrayCopy();
+        $data = array(
+            'draw' => $this->draw,
+            'recordsTotal' => $this->getNbResults(),
+            'recordsFiltered' => $this->getNbResults(),
+            'data' => $results,
+        );
+        return $data;
+    }
+    
+    function toArray($route = null,array $parameters = array(),$format = null) {
+        if($format === null){
+            $format = $this->defaultFormat;
+        }
+        if(in_array($format, $this->formatArray)){
+            $method = 'formatToArray'.ucfirst($format);
+            return $this->$method($route,$parameters);
+        }
+    }
+    
+    protected function  generateUrl($route,array $parameters){
+        return $this->container->get('router')->generate($route, $parameters, Router::ABSOLUTE_URL);
+    }
+    
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+    
+    function setDefaultFormat($defaultFormat) 
+    {
+        $this->defaultFormat = $defaultFormat;
+    }
+    
+    function setRequest(\Symfony\Component\HttpFoundation\Request $request) {
+        $this->request = $request;
+        $start = $request->get("start");
+        $length = $request->get("length");
+        $this->draw = $request->get("draw",  $this->draw) + 1;
+        if($start> 0){
+            $page = $start / $length;
+            $page = $page + 1;
+        }else{
+            $page = 1;
+        }
+        $this->setCurrentPage($page);
+        $this->setMaxPerPage($length);
+    }
+}
---
+++ ORM/EntityRepository.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnocreaciones\Bundle\ToolsBundle\ORM;
+
+use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Tecnocreaciones\Bundle\ToolsBundle\Model\Paginator\Paginator;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Doctrine\ORM\EntityRepository as Base;
+
+/**
+ * Doctrine ORM driver entity repository.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class EntityRepository extends Base implements ContainerAwareInterface
+{
+    /**
+     * @var SecurityContext
+     */
+    protected $securityContext;
+    
+    protected $container;
+    public function getPaginator(QueryBuilder $queryBuilder)
+    {
+        $pagerfanta = new Paginator(new DoctrineORMAdapter($queryBuilder));
+        $pagerfanta->setDefaultFormat($this->getFormatPaginator());
+        $pagerfanta->setContainer($this->container);
+        return $pagerfanta;
+    }
+    
+    public function findAllPaginated()
+    {
+        return $this->getPaginator($this->getQueryBuilder())
+        ;
+    }
+    
+    public function setSecurityContext(SecurityContext $securityContext) {
+        $this->securityContext = $securityContext;
+    }
+    
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+    
+    /**
+     * Get a user from the Security Context
+     *
+     * @return mixed
+     *
+     * @throws \LogicException If SecurityBundle is not available
+     *
+     * @see Symfony\Component\Security\Core\Authentication\Token\TokenInterface::getUser()
+     */
+    public function getUser()
+    {
+        if (!$this->container->has('security.context')) {
+            throw new \LogicException('The SecurityBundle is not registered in your application.');
+        }
+
+        if (null === $token = $this->container->get('security.context')->getToken()) {
+            return null;
+        }
+
+        if (!is_object($user = $token->getUser())) {
+            return null;
+        }
+
+        return $user;
+    }
+    
+    /**
+     * Retorna un paginador con valores escalares (Sin hidratar)
+     * @param \Doctrine\ORM\QueryBuilder $queryBuilder
+     * @return \Tecnocreaciones\Bundle\ResourceBundle\Model\Paginator\Paginator
+     */
+    public function getScalarPaginator(\Doctrine\ORM\QueryBuilder $queryBuilder)
+    {
+        $pagerfanta = new Paginator(new ArrayAdapter($queryBuilder->getQuery()->getScalarResult()));
+        $pagerfanta->setDefaultFormat($this->getFormatPaginator());
+        $pagerfanta->setContainer($this->container);
+        return $pagerfanta;
+    }
+    
+    public function getSecurityContext()
+    {
+        if (!$this->container->has('security.context')) {
+            throw new \LogicException('The SecurityBundle is not registered in your application.');
+        }
+
+        return $this->container->get('security.context');
+    }
+    
+    public function getFormatPaginator()
+    {
+        return Paginator::FORMAT_ARRAY_DEFAULT;
+    }
+}
