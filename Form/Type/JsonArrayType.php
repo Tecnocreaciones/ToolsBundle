8158ae5cbcfd7eb6997f24b7d79871ebd5ec8dda
Carlos Mendoza <inhack20@gmail.com>
2016-04-26 07:38:13 -0430

    Fix bug

---
+++ Form/Type/JsonArrayType.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the TecnoReady Solutions C.A. package.
+ * 
+ * (c) www.tecnoready.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnocreaciones\Bundle\ToolsBundle\Form\Type;
+
+/**
+ * TODO Tipo de dato para editar array simple en los formularios
+ * Description of JsonArrayType
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class JsonArrayType {
+    //put your code here
+}
