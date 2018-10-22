b95d130a254653290757fefc307b513de9f8a279
Carlos Mendoza <inhack20@gmail.com>
2018-10-22 17:38:34 -0400

    refactor(exporter): funcion para formatearel xml

--- Model/Exporter/Model/ModelXML.php
+++ Model/Exporter/Model/ModelXML.php
@@ -9,6 +9,8 @@
 namespace Tecnocreaciones\Bundle\ToolsBundle\Model\Exporter\Model;
 
 use Tecnocreaciones\Bundle\ToolsBundle\Model\Exporter\ModelDocument;
+use DOMDocument;
+use SimpleXMLElement;
 
 /**
  * Modelo de xml
@@ -19,5 +21,20 @@ class ModelXML extends ModelTXT
 {
    public function getFormat() {
         return "xml";
+    }
+    
+    /**
+     * Formatea el xml a formato pretty manteniendo los saltos de linea
+     * @param SimpleXMLElement $simpleXMLElement
+     * @return type
+     */
+    function formatXml(SimpleXMLElement $simpleXMLElement)
+    {
+        $xmlDocument = new DOMDocument('1.0');
+        $xmlDocument->preserveWhiteSpace = false;
+        $xmlDocument->formatOutput = true;
+        $xmlDocument->loadXML($simpleXMLElement->asXML());
+
+        return $xmlDocument->saveXML();
     }
 }
