83dfd127fc7a39c6e215c6f69e2831181fc66a66
Máximo Sojo <maxsojo13@gmail.com>
2023-01-10 16:10:10 -0400

    refactor(Liform): Se da soporte para campos ocultos (Hidden).

---
+++ Custom/Liform/Transformer/HiddenTransformer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tecnocreaciones\Bundle\ToolsBundle\Custom\Liform\Transformer;
+
+use Symfony\Component\Form\FormInterface;
+use Limenius\Liform\Transformer\AbstractTransformer;
+use Symfony\Component\Form\FormView;
+
+/**
+ * Transforma el campo oculto
+ *
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+class HiddenTransformer extends AbstractTransformer
+{
+    use \Tecnocreaciones\Bundle\ToolsBundle\Custom\Liform\CommonFunctionsTrait;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(FormInterface $form, array $extensions = [], $widget = null)
+    {
+        $this->initCommonCustom($form);
+        $formView = $this->formView;
+        
+        $schema = [
+            "type" => "hidden",
+        ];
+
+        $this->addWidget($form, $schema, false);
+        $schema = $this->addCommonSpecs($form, $schema, $extensions, $widget);
+        $schema = $this->addCommonCustom($form, $schema);
+        
+        
+        return $schema;
+    }
+}
