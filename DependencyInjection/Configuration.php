b9139a7dc91dfb69d4aeb20347159f4c08894dca
LRobles <ludwins36@gmail.com>
2026-02-10 18:27:21 -0400

    restored

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,13 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        if (method_exists(TreeBuilder::class, 'getRootNode')) {
-            $treeBuilder = new TreeBuilder('tecnocreaciones_tools');
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            $treeBuilder = new TreeBuilder();
-            $rootNode = $treeBuilder->root('tecnocreaciones_tools');
-        }
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('tecnocreaciones_tools');
         
         $rootNode
                 ->children()
